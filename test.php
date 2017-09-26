b2076d91b3f7df968ab46c044b19a67a71f88d24
delasalle-sio-martin <delasalle.sio.martin.a@gmail.com>
2017-09-26 15:52:11 +0200

    Alexis ajoute la ligne 5 au fichier test.php

--- test.php
+++ test.php
@@ -2,4 +2,5 @@
 //Création du fichier par Loïc le NUL le 26/09/2017 15:18:20
 // ajout d'une ligne par Loïc le 26/09/2017 15:19:54
 // ajout d'une ligne par Alexis le 26/09/2017 15:22:50
+// ajout d'une ligne par Alexis le 26/09/2017 15:26:00, source de conflit
 ?>

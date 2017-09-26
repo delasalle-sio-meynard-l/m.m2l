88e838850e77c6aa5738aec79b6f2c9591fd0ce5
delasalle-sio-meynard-l <delasalle.sio.meynard.l@gmail.com>
2017-09-26 16:41:33 +0200

    création attributs privé et constructeur de salle

--- modele/Salle.class.php
+++ modele/Salle.class.php
@@ -5,7 +5,17 @@
 // Création : 
 // Mise à jour : 
 
-
+class Salle
+{
+    private $id;
+    private $room_name;
+    private $capacity;
+    private $area_name;
+    
+    public function Salle($unId, $uneRoomName, $uneCapacity, $uneAreaName) {
+        
+    }
+}
 
 
 

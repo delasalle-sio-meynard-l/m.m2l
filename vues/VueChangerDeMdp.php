e1ccc2998a504ffdec875cebebee5a5c68253f9d
delasalle-sio-martin <delasalle.sio.martin.a@gmail.com>
2017-11-21 14:44:00 +0100

    Voilà t'es content? On s'en fout totalement!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ vues/VueChangerDeMdp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include_once('vues/head.php'); ?>
+	</head>
+	
+	<body>
+		<div data-role="page">
+			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
+				<h4>M2L-GRR</h4>
+				<a href="index.php?action=Menu" data-transition="<?php echo $transition; ?>">Retour menu</a>
+			</div>
+			<div  data-role="content">
+				<h4 style="text-align: center; margin-top
+			</div>
+		</div>
+	</body>
+</html>

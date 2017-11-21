2821fd8a1b0709ab224f6507c67d7e4813476194
delasalle-sio-martin <delasalle.sio.martin.a@gmail.com>
2017-11-21 16:08:32 +0100

    sdfs

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ switch($action){
 	}
 	case 'ChangerDeMdp': {
 		//include_once ('controleurs/CtrlChangerDeMdp.php'); break;
-		include_once ('controleurs/CtrlMenu.php'); break;	// ligne provisoire
+		include_once ('controleurs/CtrlChangerDeMdp.php'); break;	// ligne provisoire
 	}
 	case 'CreerUtilisateur': {
 		include_once ('controleurs/CtrlCreerUtilisateur.php'); break;

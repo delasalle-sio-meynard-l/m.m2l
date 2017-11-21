b7d4823ec7b7c44ef314aa083312b085e5ba870b
delasalle-sio-martin <delasalle.sio.martin.a@gmail.com>
2017-11-21 17:02:37 +0100

    ça marche putain!!

--- controleurs/CtrlChangerDeMdp.php
+++ controleurs/CtrlChangerDeMdp.php
@@ -6,9 +6,8 @@ if(!isset($_POST["txtNouveauMdp"]) && !isset($_POST["txtConfirmation"])) {
     $afficherMdp = 'off';
     $message = '';
     $typeMessage = '';
-    include_once('../vues/VueChangerDeMdp.php');
+    include_once('vues/VueChangerDeMdp.php');
 }
-
 else {
     if(empty($_POST["txtNouveauMdp"]) == true) $nouveauMdp = "";
         else $nouveauMdp = $_POST["txtNouveauMdp"];
@@ -17,43 +16,48 @@ else {
     if(empty($_POST["caseAfficherMdp"]) == true) $afficherMdp = 'off';
         else $afficherMdp = $_POST["caseAfficherMdp"];
         
-        $EXPRESSION = "#^(.*[0-9].*[a-z].*[A-Z].*|.*[0-9].*[A-Z].*[a-z].*|.*[a-z].*[A-Z].*[0-9].*|.*[a-z].*[0-].*[A-Z].*|.*[A-Z].*[0-9].*[a-z].*|.*[A-Z].*[a-z].*[0-9].*)$#";
-        if(preg_match($EXPRESSION, $nouveauMdp) == false || strlen($nouveauMdp) <8) {
+        //$EXPRESSION = "#^(.*[0-9].*[a-z].*[A-Z].*|.*[0-9].*[A-Z].*[a-z].*|.*[a-z].*[A-Z].*[0-9].*|.*[a-z].*[0-].*[A-Z].*|.*[A-Z].*[0-9].*[a-z].*|.*[A-Z].*[a-z].*[0-9].*)$#";
+        $EXPRESSION = "^[a-zA-Z0-9]+";
+        if(strlen($nouveauMdp) <5) {
             $message = 'Données incomplètes';
             $typeMessage = 'avertissement';
-            include_once('../vues/VueChangerDeMdp.php');
+            include_once('vues/VueChangerDeMdp.php');
         }
         else {
             if($nouveauMdp != $confirmationMdp) {
                 $message = 'Le nouveau mot de passe et<br>sa confirmation sont différents';
                 $typeMessage = 'avertissement';
-                include_once('../vues/VueChangerDeMdp.php');
+                //include_once('vues/VueChangerDeMdp.php');
             }
             else {
+                include_once ('modele/DAO.class.php');
+                $dao = new DAO();
+                $dao->modifierMdpUser($nom, $nouveauMdp);
+                
                 $sujet = "Modification de votre mot de passe";
                 $message = "Votre mot de passe a été modifié. \n\n";
                 $message.= "Votre mot de passe est : " .$nouveauMdp;
-                $adresseEmetteur = "delasalle.sio.eleves@gmail.com";
-                $adresseDestinataire = "delasalle.sio.meynard.l@gmail.com";
+                //$adresseEmetteur = "delasalle.sio.eleves@gmail.com";
+                //$adresseDestinataire = "delasalle.sio.meynard.l@gmail.com";
                 
-                if(preg_match("#^.+gmail\.com$#", $adresseDestinataire) == true) {
-                    $adresseDestinataire = str_replace(".", $adresseDestinataire);
-                    $adresseDestinataire = str_replace("@gmailcom", "@gmail.com", $adresseDestinataire);
-                }
+                $adrEmail = $dao->getEmailUtilisateur($nom);
                 
-                $ok = mail($adresseDestinataire, $sujet, $message, "From: ".adresseEmetteur);
+                $ok = mail($adrEmail, $sujet, $message, "From: delasalle.sio.crib@gmail.com");
                 
                 if($ok) {
                     $message = "Enregistrement effectué.<br>Vous allez recevoir un mail de confirmation.";
                     $typeMessage = 'information';
+                    $themeFooter = $themeNormal;
+                    include_once('vues/VueChangerDeMdp.php');
                 }
                 
                 else {
                     $message = "Enregistrement effectué.<br>L'envoi du mail de confirmation a rencontré un problème.";
                     $typeMessage = 'avertissement';
+                    $themeFooter = $themeProbleme;
+                    include_once ('vues/VueChangerDeMdp.php');
                 }
                 
-                include_once('../vues/VueChangerDeMdp.php');
                 
             }
         }
--- vues/VueChangerDeMdp.php
+++ vues/VueChangerDeMdp.php
@@ -8,12 +8,57 @@
 			
 			// associe une fonction à l'événement pageinit
 			$(document).bind('pageinit', function() {
+				$('#caseAfficherMdp').click(afficherMdp);
+
+				//$('#formModificationMdp').submit(validationGenerale);
 				
-				<?php if ($typeMessage != '') { ?>
-					// affiche la boîte de dialogue 'affichage_message'
-					$.mobile.changePage('#affichage_message', {transition: "<?php echo $transition; ?>"});
+				//alert("<?php echo $typeMessage; ?>");
+				//alert("<?php echo $message; ?>");
+				//alert("<?php echo $nouveauMdp; ?>");
+				//alert("<?php echo $confirmationMdp; ?>");
+				
+				<?php if($typeMessage == 'avertissement') { ?>
+					afficher_avertissement("<?php echo $message; ?> ");
 				<?php } ?>
-			} );
+
+				<?php if($typeMessage == 'information') { ?> 
+					afficher_information("<?php echo $message; ?> ");
+				<?php } ?>
+
+			});
+
+			function afficherMdp() {
+				if($("#caseAfficherMdp").is(":checked")){
+					$('#txtNouveauMdp').attr('type', 'text');
+					$('#txtConfirmation').attr('type', 'text');
+				}
+				else {
+					$('#txtNouveauMdp').attr('type', 'password');
+					$('#txtConfirmation').attr('type', 'password');
+				};
+			}
+
+			function validationGenerale() {
+				if($('#txtNouveauMdp').val() != $('#txtConfirmation').val() ){
+					afficher_avertissement("Les 2 valeurs saisies sont différentes !");
+					return false;
+				}
+
+				return true;
+			}
+			function afficher_information(msg) {
+				$('#texte_message_information').empty();
+				$('#texte_message_information').append(msg);
+				$.mobile.changePage('#affichage_message_information', {transition: "flip"});
+				//alert(msg);
+			}
+
+			function afficher_avertissement(msg) {
+				$('#texte_message_avertissement').empty();
+				$('#texte_message_avertissement').append(msg);
+				$.mobile.changePage('#affichage_message_avertissement', {transition: "flip"});
+				//alert(msg);
+			}
 
 		</script>
 	</head>
@@ -28,14 +73,14 @@
 				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;">Changer mon mot de passe</h4>
 				
 				<form name="form1" action="index.php?action=ChangerDeMdp" data-ajax="false" method="post" data-transition="<?php echo $transition; ?>">
-					<div data-role="fieldcontain" class="ui-hide-label">
+					<div data-role="fieldcontain">
 						<label for="txtNouveauMdp">Nouveau mot de passe :</label>
-						<input type="<?php if($afficherMdp == 'off') echo 'password'; else echo 'text' ;?>" name="txtNouveauMdp" id="txtNouveauMdp" data-mini="true" placeholder="Mon nouveau mot de passe" required pattern="^.{8,}$" value = "<?php  echo $nouveauMdp; ?>">
+						<input type="<?php if($afficherMdp == 'off') echo 'password'; else echo 'text' ;?>" name="txtNouveauMdp" id="txtNouveauMdp" data-mini="true" placeholder="Mon nouveau mot de passe" required pattern="^.{5,}$" value = "<?php  echo $nouveauMdp; ?>">
 					</div>
 					
 					<div data-role="fieldcontain">
 						<label for="txtConfirmation">Confirmation nouveau mot de passe:</label>
-						<input type="<?php if($afficherMdp =='off') echo 'password'; else echo 'text';?>" name="txtConfirmation" id="txtConfirmation" placeholder="Confirmation de mon nouveau mot de passe" required pattern = "^.{8,}$" value="<?php echo $confirmationMdp; ?>">
+						<input type="<?php if($afficherMdp =='off') echo 'password'; else echo 'text';?>" name="txtConfirmation" id="txtConfirmation" placeholder="Confirmation de mon nouveau mot de passe" required pattern = "^.{5,}$" value="<?php echo $confirmationMdp; ?>">
 					</div>
 					
 					<div data-role="fieldcontain" data-type="horizontal" class="ui-hide-label">

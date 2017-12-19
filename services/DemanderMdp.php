e6501d72b93e3e6c001efff646a83c5d98a993aa
delasalle-sio-martin <delasalle.sio.martin.a@gmail.com>
2017-12-19 14:50:49 +0100

    Test

--- services/DemanderMdp.php
+++ services/DemanderMdp.php
@@ -19,42 +19,47 @@ function chaine_aleatoire($nb_car, $chaine = 'azertyuiopqsdfghjklmwxcvbn12345678
 
     // Récupération des données transmises
     // la fonction $_GET récupère une donnée passée en paramètre dans l'URL par la méthode GET
-    if ( empty ($_GET ["nomUtilisateur"]) == true)  $nomUtilisateur = "";  else   $nomUtilisateur = $_GET ["nom"];
-
+    if ( empty ($_GET ["nom"]) == true)  $nom = "";  else   $nom = $_GET ["nom"];
+    
     // si l'URL ne contient pas les données, on regarde si elles ont été envoyées par la méthode POST
     // la fonction $_POST récupère une donnée envoyées par la méthode POST
-    if ( $nomUtilisateur == "")
-        {	if ( empty ($_POST ["nomUtilisateur"]) == true)  $nomUtilisateur = "";  else   $nomUtilisateur = $_POST ["nom"];
-        }
+    if ( $nom == "")
+    {	if ( empty ($_POST ["nom"]) == true)  $nom = "";  else   $nom = $_POST ["nom"];
+    }
     
-        if($nomUtilisateur == "") {
-            $message = "Erreur : Données incomplètes";
-        } 
+    if($nom == "") {
+        $message = "Erreur : Données incomplètes";
+    }
+    else {
+        include_once ('../modele/DAO.class.php');
+        $dao = new DAO();
+        $unUtilisateur = $dao->getUtilisateur($nom);
+        
+        if ($unUtilisateur == null) {
+            $message = "Erreur : nom d'utilisateur inexistant.";
+        }
+        
         else {
-            include_once ('../modele/DAO.class.php');
-            $dao = new DAO();
+            $adrMail = $unUtilisateur->getEmail();
+            $password = Outils::creeMdp();
+            $dao->modifierMdpUser($nom, $password);
+            $level = $dao->getNiveauUtilisateur($nom, $password);
             
-            if ($dao->getUtilisateur($nomUtilisateur) == null) {
-                $message = "Erreur : nom d'utilisateur inexistant.";
+            $sujet = "Votre nouveau mot de passe";
+            $contenuMail = "Voici les nouvelles données vous concernant :\n\n";
+            $contenuMail .="Votre mot de passe : " . $password . " (nous vous conseillons de le changer)\n";
+            $contenuMail .="Votre niveau d'accès : " . $level . "\n";
+            
+            $ok = Outils::envoyerMail($adrMail, $sujet, $contenuMail, $ADR_MAIL_EMETTEUR);
+            if(! $ok) {
+                $message = "Erreur : Echec lors de l'envoi du mail.";
             }
             
             else {
-                $mdpDemande = chaine_aleatoire(5);
-                
-                $dao->modifierMdpUser($nomUtilisateur, $mdpDemande);
-                $adrEmail = $dao->getEmailUtilisateur($nomUtilisateur);
-                
-                $ok = $out->envoyerMail($adrEmail, $sujet, $contenuMail, "delasalle.sio.crib@gmail.Com");
-                
-                if(! $ok) {
-                    $message = "Erreur : Echec lors de l'envoi du mail.";
-                }
-                
-                else {
-                    $message = "Vous allez recevoir un mail avec votre nouveau mot de passe.";
-                }
+                $message = "Vous allez recevoir un mail avec votre nouveau mot de passe.";
             }
         }
+    }
         
         unset($dao);
         

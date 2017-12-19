7dd87a27e9fe7f578087baec355ebc16f623d186
delasalle-sio-martin <delasalle.sio.martin.a@gmail.com>
2017-12-19 15:14:53 +0100

    Données incomplètes

--- services/ChangerDeMdp.php
+++ services/ChangerDeMdp.php
@@ -1,1 +1,18 @@
 <?php
+    
+
+    include_once ('../modele/Outils.class.php');
+    include_once ('../modele/parametres.localhost.php');
+    
+    if ( empty ($_GET["nouveauMdp"]) == true) $nouveauMdp = ""; else $nouveauMdp = $_GET["nom"];
+    if ( empty ($_GET["confirmationMdp"]) == true) $confirmationMdp = ""; else $confirmationMdp = $_GET["confirmationMdp"];
+    
+    if ($nouveauMdp == "" && $confirmationMdp == "")
+    {
+        if (empty($_POST["nouveauMdp"]) == true) $nouveauMdp = ""; else $nouveauMdp = $_POST["nouveauMdp"];
+        if (empty($_POST["confirmationMdp"]) == true) $confirmationMdp = ""; else $confirmationMdp = $_POST["confirmationMdp"];
+    }
+    
+    if ($nouveauMdp == "" && $confirmationMdp == "") {
+        $message = "Erreur : Données incomplètes";
+    }

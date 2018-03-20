0eb5a4e661332c335788e96c1b25714dd17ec503
delasalle-sio-martin <delasalle.sio.martin.a@gmail.com>
2018-03-20 15:14:46 +0100

    Commit changerDeMdp

--- services/ChangerDeMdp.php
+++ services/ChangerDeMdp.php
@@ -4,19 +4,23 @@
     include_once ('../modele/parametres.localhost.php');
     
     if ( empty ($_REQUEST['nom']) == true) $nom = ""; else $nom = $_REQUEST["nom"];
-    if ( empty ($_REQUEST["mdp"]) == true) $mdp = ""; else $mdp = $_REQUEST["mdp"];
-    if ( empty ($_REQUEST["txtNouveauMdp"]) == true) $nouveauMdp = ""; else $nouveauMdp = $_REQUEST["txtNouveauMdp"];
-    if ( empty ($_REQUEST['txtConfirmation']) == true) $confirmationMdp = ""; else $confirmationMdp = $_REQUEST['txtConfirmation'];
+    if ( empty ($_REQUEST["ancienMdp"]) == true) $ancienMdp = ""; else $ancienMdp = $_REQUEST["ancienMdp"];
+    if ( empty ($_REQUEST["nouveauMdp"]) == true) $nouveauMdp = ""; else $nouveauMdp = $_REQUEST["nouveauMdp"];
+    if ( empty ($_REQUEST['confirmationMdp']) == true) $confirmationMdp = ""; else $confirmationMdp = $_REQUEST['confirmationMdp'];
+    if ( empty ($_REQUEST['lang']) == true) $lang = ""; else $lang = $_REQUEST["lang"];
+    
     if ($lang != "json") $lang="xml";
+    
+    include_once('../modele/DAO.class.php');
     
     $dao = new DAO();
     
-    if ($nouveauMdp == "" || $confirmationMdp == "" || $mdp = "" || $nom = "") {
+    if ($nouveauMdp == "" || $confirmationMdp == "" || $ancienMdp = "" || $nom = "") {
         $message = "Erreur : Données incomplètes";
     }
     else
     {
-        if ($dao->getNiveauUtilisateur($nom, $mdp) == "inconnu")
+        if ($dao->getNiveauUtilisateur($nom, $ancienMdp) == "inconnu")
             $msg = "Erreur : authentification incorrecte.";
         
             else {
@@ -37,4 +41,22 @@
     
     function creerFluxXML($message) {
         
+        $doc = new DOMDocument();
+        
+        $doc->version = '1.0';
+        $doc->encoding = 'ISO-8859-1';
+        
+        $elt_commentaire = $doc->createComment('Service web ChangerDeMdp - BTS SIO - Lycée De La Salle - Rennes');
+        $doc->appendChild($elt_commentaire);
+        
+        $elt_data = $doc->createElement('data');
+        $doc->appendChild($elt_data);
+        
+        $elt_reponse = $doc->createElement('reponse', $message);
+        $elt_data->appendChild($elt_reponse);
+        
+        $doc->formatOutput = true;
+        
+        echo $doc->saveXML();
+        return;
     }
